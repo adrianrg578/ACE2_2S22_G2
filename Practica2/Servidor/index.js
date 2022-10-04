abb4ee294202c7c9b5b6bdf4548dac2cf324d443
adrianrg578 <201404297@ingenieria.usac.edu.gt>
2022-10-04 14:25:11 -0600

    servidor

---
+++ Practica2/Servidor/index.js
@@ -0,0 +1,40 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const app = express();
+const {createServer} = require("http");
+const {Server} =  require("socket.io")
+const cors = require ("cors");
+app.use(cors());
+
+const server = {createServer}.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+    }
+});
+
+const port = process.env.PORT||4001;
+
+const {SerialPort} = require('serialport');
+const {ReadlineParser}=require('@serialport/parser-readline')
+
+//Puerto que le asigno la PC al BT 
+const mySerial = new SerialPort({path:'COM6',baudRate:9600});
+
+const parser = mySerial.pipe(new ReadlineParser({ delimiter:'\n\r'}))
+
+var data_arduino;
+
+parser.on('data',function(data){
+    let temp = data.toString();
+    const datos = temp.split(",");
+    if(datos[1]){
+        data_arduino = {BPM: datos[0], spo2: datos[1], distancia: datos[2], repeticiones: datos[3]};
+        console.log(data_arduino);   
+    }
+
+});
+
+server.listen(4001,()=>{console.log('servidor en el puerto ',4001);
+});
