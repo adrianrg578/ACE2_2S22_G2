c230c483ce43644d8e5aa874a9d7572070f7ed97
adrianrg578 <201404297@ingenieria.usac.edu.gt>
2023-01-24 09:48:45 -0600

    Borrado de credenciales Base de Datos

--- Proyecto2/servidor/db2.js
+++ Proyecto2/servidor/db2.js
@@ -1,9 +1,13 @@
 const mysql = require("mysql2/promise");
 
-module.exports = mysql.createConnection({
-    host: '35.232.210.160',
-    user: 'root',
-    password: '1234',
-    database: 'jump_box',
-    port: '3306'
-});
+module.exports = mysql.createConnection(
+ // ingresar los datos proporcionados para el funcionamiento de la base
+ {  /*
+ host: '',
+ user: '',
+ password: '',
+ database: '',
+ port: ''
+ */
+}
+);
